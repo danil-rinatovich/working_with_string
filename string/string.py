9702bdb8d514d4d92155ea63df5397256c3656d0
Danil Ibatullin <danil_rinatovich@list.ru>
2024-09-22 22:15:22 +0500

    variables

--- string/string.py
+++ string/string.py
@@ -1,6 +1,17 @@
-str = 'example'
-print(str[0])
-print(str[-1])
-print(str[4:])
-print(str[::-1])
-print(str[1::2])
+#task_first
+    #str = 'example'
+    #print(str[0])
+    #print(str[-1])
+    #print(str[4:])
+    #print(str[::-1])
+    #print(str[1::2])
+
+#task_second
+count_completed_home_work = 12
+count_hours_spent = 1.5
+course_name = 'Python'
+time_one_task = 1.5 / 12
+print('Курс:', course_name, ",",
+      'всего задач:', count_completed_home_work, ",",
+      'затрачено часов:', count_hours_spent, ",",
+      'среднее время выполнения', time_one_task, 'часа.')
