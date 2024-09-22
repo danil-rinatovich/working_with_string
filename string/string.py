293c4010bef96aa6257b9fe6a47989cef06d8e3e
Danil Ibatullin <danil_rinatovich@list.ru>
2024-09-22 22:46:33 +0500

    variable time_one_task

--- string/string.py
+++ string/string.py
@@ -10,7 +10,7 @@
 count_completed_home_work = 12
 count_hours_spent = 1.5
 course_name = 'Python'
-time_one_task = 1.5 / 12
+time_one_task = count_hours_spent / count_completed_home_work
 print('Курс:', course_name, ",",
       'всего задач:', count_completed_home_work, ",",
       'затрачено часов:', count_hours_spent, ",",
